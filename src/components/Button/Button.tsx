c2f9ab26bf29975bacf1465addc48f8693f7bfba
ashlapak-cds <anastasiia@cognitive-design-systems.com>
2025-11-25 12:14:15 +0100

    (fix) fix button to have default properties

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,25 +1,27 @@
 import classNames from "classnames";
 import styles from "./Button.module.scss";
+import React from "react";
 
-type ButtonProps = {
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   variant?: "primary" | "secondary" | "primary-no-bg";
   size?: "sm" | "md" | "lg";
-  children: React.ReactNode;
-  onClick?: () => void;
 };
 
 export default function Button({
   variant = "primary",
   size = "md",
-  children,
-  onClick,
+  className,
+  ...props
 }: ButtonProps) {
   return (
     <button
-      className={classNames(styles.button, styles[variant], styles[size])}
-      onClick={onClick}
-    >
-      {children}
-    </button>
+      className={classNames(
+        styles.button,
+        styles[variant],
+        styles[size],
+        className
+      )}
+      {...props}
+    />
   );
 }
